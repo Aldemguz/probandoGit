b01f183dae0be986352b39979d302bc3a7c0b29b
Aldemaro Guzman <aldemaroguzman93@gmail.com>
2018-08-19 21:53:13 -0430

    First commit, before use Github, Hello world

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+Testing Git and github in a project.
+
+by: Aldemaro Guzman
+
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
